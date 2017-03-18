09c2ac345541b78cdaaaabc1ddc8ea603d1ff346
pashakhromov <pasha.khromov@gmail.com>
2017-03-18 23:48:05 +0300

    Update grad_boost.py
    Adjustments in formatting

--- grad_boost.py
+++ grad_boost.py
@@ -19,11 +19,11 @@ if __name__ == '__main__':
 
 	# remove features with lookahead bias
 	drop_col = [	'duration',
-					'radiant_win',
-					'tower_status_radiant',
-					'tower_status_dire',
-					'barracks_status_radiant',
-					'barracks_status_dire']
+			'radiant_win',
+			'tower_status_radiant',
+			'tower_status_dire',
+			'barracks_status_radiant',
+			'barracks_status_dire']
 	data.drop(drop_col, 1, inplace=True)
 
 	# NA->0 althought there's probably a better choice
@@ -43,15 +43,15 @@ if __name__ == '__main__':
 		start_time = datetime.datetime.now()
 		
 		clf = GradientBoostingClassifier(	n_estimators=n_est, 
-											random_state=seed,
-											subsample=1.0,
-											max_depth=3)
+							random_state=seed,
+							subsample=1.0,
+							max_depth=3)
 		
 		cv = cross_val_score(	estimator=clf,
-								X=X,
-								y=y,
-								scoring='roc_auc',
-								cv=kf)
+					X=X,
+					y=y,
+					scoring='roc_auc',
+					cv=kf)
 		
 		print '\n#Estimators = %d\nCross-validation score (mean) = %0.5f\nTime (sec): %0.1f' % (n_est, cv.mean(), (datetime.datetime.now() - start_time).total_seconds())
 		
